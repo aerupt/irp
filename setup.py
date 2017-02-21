683eedf48f5250ad86f6969f2f82b04a8490d5f9
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-02-21 17:06:34 +0100

    Release 0.3.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as readme:
 
 setup(
     name='irp',
-    version='0.2.0',
+    version='0.3.0',
     description='Gets possible airports for city names!',
     long_description=long_description,
     url='https://github.com/aerupt/irp',
